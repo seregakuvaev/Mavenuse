53acefba6e88353022acb87a9240c7ed65dd5f46
seregakuvaev <seregakuvaev@mail.ru>
2022-08-05 14:35:23 +0300

    final1

--- src/main/java/com/jdbc/model/User.java
+++ src/main/java/com/jdbc/model/User.java
@@ -9,7 +9,9 @@ public class User {
 
     @Override
     public String toString() {
-        return "User {name: " + name + "}";
+        return "User{" +
+                "name='" + name + '\'' +
+                '}';
     }
 
 }
