0197ba8bff035c717b6d3d18c459b9fdc78a912a
seregakuvaev <seregakuvaev@mail.ru>
2022-09-02 19:20:28 +0300

    final1

--- src/main/java/com/jdbc/util/UtilHiber.java
+++ src/main/java/com/jdbc/util/UtilHiber.java
@@ -3,15 +3,9 @@ package com.jdbc.util;
 import com.jdbc.model.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class UtilHiber {
 
@@ -23,19 +17,6 @@ public class UtilHiber {
 
     private void buildSessionFactory() {
         try {
-
-            //Map<String, Object> settings = new HashMap<>();
-            ///settings.put("connection.driver_class", "com.mysql.jdbc.Driver");
-            //settings.put("dialect", "org.hibernate.dialect.MySQL8Dialect");
-            //settings.put("hibernate.connection.url",
-            //        "jdbc:mysql://localhost:3306/test1");
-            //settings.put("hibernate.connection.username", "root");
-            //settings.put("hibernate.connection.password", "root");
-            //settings.put("hbm2ddl.auto", "create-drop");
-            //settings.put("hibernate.current_session_context_class", "thread");
-            //settings.put("hibernate.show_sql", "true");
-            //settings.put("hibernate.format_sql", "true");
-
             Configuration configuration = new Configuration();
 
             configuration.setProperty("connection.driver_class", "com.mysql.jdbc.Driver");
@@ -56,16 +37,6 @@ public class UtilHiber {
 
 
             sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-            //ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-            //        .applySettings(settings).build();
-
-           // MetadataSources metadataSources = new MetadataSources(serviceRegistry);
-
-            //metadataSources.addAnnotatedClass(User.class);
-            //Metadata metadata = metadataSources.buildMetadata();
-
-
-            //sessionFactory = metadata.getSessionFactoryBuilder().build();
         } catch (Exception e) {
             e.printStackTrace();
         }
